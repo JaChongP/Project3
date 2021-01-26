39026ea67d6b7fbe88feafc9f1cc6d7f2c6e0d50
Your Name <julianabjortiv@gmail.com>
2021-01-26 23:47:22 +0000

    added onboarding tutorial page

--- Greenify/script.js
+++ Greenify/script.js
@@ -34,3 +34,59 @@ function isElementInViewport(el) {
   }
 
 
+
+
+
+  
+
+var tabs = document.querySelectorAll("[id^='tabHeader_']");
+var tabPages = document.getElementsByClassName("tabpage");
+
+for (var i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function(e) {
+    var id = e.target.id.split("_")[1];
+    switchTab(id);
+  });
+}
+
+function switchTab(id) {
+  id = parseInt(id);
+  var activeTab = document.getElementById("tabHeader_" + id);
+  var tabPageId;
+  var i;
+
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("tabActiveHeader");
+    tabs[i].parentNode.removeAttribute("data-current");
+  }
+
+  activeTab.parentNode.setAttribute("data-current", id);
+  activeTab.classList.add("tabActiveHeader");
+
+  for (i = 0; i < tabPages.length; i++) {
+    tabPageId = parseInt(tabPages[i].id.split("_")[1]);
+    if (tabPageId === id) {
+      tabPages[i].style.display = "block";
+    } else {
+      tabPages[i].style.display = "none";
+    }
+  }
+}
+
+function prevTab(){
+  var nextTab;
+  var currentTab = document.querySelectorAll("[data-current]")[0].getAttribute("data-current");
+  if(currentTab > 1){
+  	switchTab(currentTab-1);
+  }  
+}
+
+function nextTab(){
+  var nextTab;
+  var currentTab = document.querySelectorAll("[data-current]")[0].getAttribute("data-current");
+  if(currentTab < tabs.length){
+  	switchTab(parseInt(currentTab)+1);
+  }  
+}
+
+
