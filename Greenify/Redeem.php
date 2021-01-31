8c9c9c9999bb6e4e142c09db575abf9a73cc79c9
Your Name <julianabjortiv@gmail.com>
2021-01-31 14:15:50 +0000

    trying include header

--- Greenify/Redeem.php
+++ Greenify/Redeem.php
@@ -8,28 +8,7 @@
 </head>
 <body class="user-page">
 
-   <header>    
-            <nav>
-                </div>
+<?php include "./usernav.html" ?>
 
-                <div class="side-nav">
-                <ul class="side-nav">
-                    <li><div class="logo-white">
-                        <img src="images/logo-white.png">
-                    </div></li>
-                    <li><a href="User.php"><img src="images/icon-home.png" class="small-icon">Dashboard</a></li>
-                    <li><a href="ShopInfo.php"><img src="images/icon-stores.png" class="small-icon">Stores</a></li>
-                    <li><a href=""><img src="images/icon-redeem.png" class="small-icon">Redeem</a></li>
-                    <li><a href=""><img src="images/icon-guide.png" class="small-icon">Guide</a></li>
-                    <li><a href=""><img src="images/icon-help.png" class="small-icon">Help</a></li>
-                  </ul>
-                </div>
-            
-            </nav>
-
-            <div class="top-nav-user">
-                <ul class="top-nav-user">
-                    <li><a href="LogOut.php">Sign Out</a></li>
-                </ul>
-            </div>
-</header>
+</body>
+</html>
