f508750cab95c5bc372c86b8aa9eea6a70c1f342
Josean Chong Portillo <73476640+JaChongP@users.noreply.github.com>
2021-02-06 12:10:44 +0000

    con

--- Greenify/includes/userManagement/completetasks.php
+++ Greenify/includes/userManagement/completetasks.php
@@ -2,6 +2,7 @@
 
     session_start();
     include './database.php';
+    include 'uservar.php';
 
 
     $sqlASS = "SELECT DISTINCT task_id FROM assigned_tasks WHERE user_id = ".$_SESSION['id']." AND completed = 0";
@@ -29,7 +30,7 @@
 
         $sqlUPDATE = "UPDATE assigned_tasks SET completed = 1, completed_date = CURRENT_TIMESTAMP() WHERE task_id = $taskID";
         $resultUPDATE = $conn->query($sqlUPDATE);
-        $sqlUPDATE_2 = "UPDATE users SET user_experience = user_experience + $ct_exp, user_coins = user_coins + $ct_coins WHERE user_id = 2";
+        $sqlUPDATE_2 = "UPDATE users SET user_experience = user_experience + $ct_exp, user_coins = user_coins + $ct_coins WHERE user_id = ".$user['user_id']."";
         $resUPDATE_2 = $conn->query($sqlUPDATE_2);
 
         header("location: ../home.php");
