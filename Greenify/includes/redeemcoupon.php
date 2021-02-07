bb92c4c9b78d98ce7001bab8d5fa750e3502ffce
Josean Chong Portillo <73476640+JaChongP@users.noreply.github.com>
2021-02-07 01:23:37 +0000

    Update redeemcoupon.php

--- Greenify/includes/redeemcoupon.php
+++ Greenify/includes/redeemcoupon.php
@@ -6,13 +6,15 @@
 
     $userID = $user['user_id'];
 
-
     if(isset($_POST['redeem'])){
         $coupon_id = $_POST['redeem'];
 
         $sql_in = "INSERT INTO user_redeem (coupon_id, user_id) VALUES ($coupon_id, $userID)";
         $result_in = $conn->query($sql_in);
-        $conn->close();
+
+
+        $sql_ud = "UPDATE users SET user_coins = user_coins - (SELECT coupon_cost FROM redeem WHERE coupon_id= $coupon_id) WHERE user_id = $userID";
+        $result_ud = $conn->query($sql_ud);
 
         header("location: ../User.php");
         exit();
