c4acb8f416683b4458ab98fce6cb739d2337535a
julianabjort <57215398+julianabjort@users.noreply.github.com>
2021-02-07 12:12:04 +0000

    Create TaskProcess.php

---
+++ Greenify/includes/TaskProcess.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'includes/database.php';
+
+if(sizeof($_POST['task'])>0){ //eru gögn?
+    foreach($_POST['task'] as $task) {
+        $sql = "INSERT INTO users_tasks (user_id, task_id) ";
+        $sql .= "VALUES(".$_POST["user_id"].",".$task."); ";
+        if ($conn->query($sql) === FALSE) {
+            die($conn->error);
+          }
+
+    }
+}
+
+header("Location:/Greenify/User.php");
+
+ //echo "<pre>"; print_r ($_POST); die();
